13d5c730d7ca0b49e4d5ba77e5787457956ee893
MihajaRkt <rktmihaja8@gmail.com>
2026-07-20 17:13:10 +0300

    s

--- base.sql
+++ base.sql
@@ -98,7 +98,7 @@ CREATE TABLE operations(
     idType_operation INT REFERENCES type_operation(idType_operation),
     idFrais INT REFERENCES frais(idFrais),
     idUtilisateur INT REFERENCES utilisateurs(idUtilisateur),
-    destinataire varchar(30),
+    idDestinataire INT REFERENCES utilisateurs(idUtilisateur),
     date_operation DATE,
     montant decimal(10,2)
 );
@@ -112,6 +112,15 @@ INSERT INTO operations (idOperateur, idType_operation, idFrais, idUtilisateur, d
 (2, 1, 9, 4, '2026-07-15', 600000.00),
 (1, 3, 10, 3, '2026-07-18', 1500000.00),
 (2, 1, 4, 1, '2026-07-20', 15000.00);
+INSERT INTO operations (idOperateur, idType_operation, idFrais, idUtilisateur, date_operation, montant, idDestinataire) VALUES
+(1, 1, 2, 1, '2026-07-01', 5000.00, 2),
+(2, 2, 4, 2, '2026-07-02', 20000.00, 2),
+(4, 2, 5, 3, '2026-07-05', 50000.00, 3),
+(2, 1, 7, 4, '2026-07-10', 150000.00, 4),
+(1, 2, 1, 5, '2026-07-12', 500.00, 3),
+(5, 1, 9, 4, '2026-07-15', 600000.00, 3),
+(3, 2, 10, 3, '2026-07-18', 1500000.00, 4),
+(2, 1, 4, 1, '2026-07-20', 15000.00, 5);
 
 CREATE TABLE solde(
     idSolde INTEGER PRIMARY KEY AUTOINCREMENT,
