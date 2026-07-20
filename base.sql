69c1cae5791d26ed3bca93e619c01448bf1780f5
MihajaRkt <rktmihaja8@gmail.com>
2026-07-20 08:49:05 +0300

    Base V1

---
+++ base.sql
@@ -0,0 +1,59 @@
+DROP TABLE IF EXISTS operateurs;
+
+DROP TABLE IF EXISTS type_operation;
+
+DROP TABLE IF EXISTS frais;
+
+DROP TABLE IF EXISTS operations;
+
+DROP TABLE IF EXISTS utilisateurs;
+
+CREATE TABLE operateurs(
+    idOperateur INTEGER PRIMARY KEY AUTOINCREMENT,
+    prefixe varchar(10),
+    nom varchar(50)
+);
+
+INSERT INTO operateurs(prefixe, nom) VALUES
+('032', 'Orange'),
+('033', 'Airtel'),
+('037', 'Orange'),
+('034', 'Yas'),
+('038', 'Yas');
+
+CREATE TABLE type_operation(
+    idType_operation INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom varchar(50)
+);
+
+INSERT INTO type_operation(nom) VALUES
+('depot'),
+('retrait'),
+('transfert');
+
+
+CREATE TABLE frais(
+    idFrais INTEGER PRIMARY KEY AUTOINCREMENT,
+    description varchar(100),
+    montant decimal(10,2)
+);
+
+CREATE TABLE operations(
+    idOperation INTEGER PRIMARY KEY AUTOINCREMENT,
+    idOperateur INT REFERENCES operateurs(idOperateur),
+    idType_operation INT REFERENCES type_operation(idType_operation),
+    idFrais INT REFERENCES frais(idFrais),
+    montant decimal(10,2)
+);
+
+CREATE TABLE utilisateurs(
+    idUtilisateur INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom varchar(75),
+    telephone varchar(30),
+    motdepasse varchar(20)
+);
+
+INSERT INTO utilisateurs(nom, telephone, motdepasse) VALUES
+('Alice', '0320000000', 'a'),
+('Admin', '0330000000', 'a');
+
